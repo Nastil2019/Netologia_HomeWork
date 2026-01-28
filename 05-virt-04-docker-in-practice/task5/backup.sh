66282d6f1f8f68670c33a72a3e173ed016b7f774
Nastil2019 <nastilvasileva@gmail.com>
2026-01-28 18:52:59 +0000

    add task5 folder

--- 05-virt-04-docker-in-practice/task5/backup.sh
+++ 05-virt-04-docker-in-practice/task5/backup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e
+
 BACKUP_DIR="/opt/backup"
+DATE=$(date +%F_%H-%M)
+BACKUP_FILE="$BACKUP_DIR/backup_$DATE.sql"
+
 mkdir -p "$BACKUP_DIR"
+
+# Запускаем mysqldump ВНУТРИ контейнера, но вывод направляем НА ХОСТ
 docker run --rm \
-  --network my-solution_backend \
-  -v "$BACKUP_DIR":/backup \
-  -e MYSQL_PWD=secret123 \
+  --network task5_backend \
+  -e MYSQL_PWD=very_strong \
   mysql:8 \
-  mysqldump -h 172.20.0.10 -u root --all-databases > "/backup/backup_$(date +%F_%H-%M).sql"
-echo "Backup created: $(ls -1 /opt/backup/ | tail -1)"
+  mysqldump -h 172.20.0.10 -u app --all-databases > "$BACKUP_FILE"
+
+echo "[$(date)] Backup created: $BACKUP_FILE"
